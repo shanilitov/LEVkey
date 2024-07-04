bf3c98eb1715579c830f9ec71bdeb323459f694f
miryamiayafa <miryam7.52002@gmail.com>
2024-07-05 02:07:28 +0300

    Request API

--- mesh_app/src/assets/components/WorldMapComponent.jsx
+++ mesh_app/src/assets/components/WorldMapComponent.jsx
@@ -1,13 +1,30 @@
-// src/assets/components/WorldMapComponent.jsx
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import worldMapImage from '../img/MapImg..jpg';
 
 const WorldMapComponent = ({ latitude, longitude }) => {
   const mapRef = useRef(null);
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
+    // Fetch locations from server
+    const fetchLocations = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/get_locations');
+        if (response.ok) {
+          const data = await response.json();
+          setLocations(data);
+        } else {
+          console.error('Failed to fetch locations');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchLocations();
+
     // Initialize map
     mapRef.current = L.map('map', {
       center: [latitude, longitude], // Initial center of the map
@@ -22,17 +39,20 @@ const WorldMapComponent = ({ latitude, longitude }) => {
       maxZoom: 19,
     }).addTo(mapRef.current);
 
-    // Add marker based on provided latitude and longitude
-    if (latitude && longitude) {
-      const marker = L.marker([latitude, longitude]).addTo(mapRef.current);
-      marker.bindPopup('<b>Your Location</b>').openPopup();
-    }
-
     return () => {
       // Clean up map instance
       mapRef.current.remove();
     };
   }, [latitude, longitude]);
+
+  useEffect(() => {
+    if (mapRef.current && locations.length > 0) {
+      locations.forEach(({ latitude, longitude }) => {
+        const marker = L.marker([latitude, longitude]).addTo(mapRef.current);
+        marker.bindPopup('<b>Device Location</b>').openPopup();
+      });
+    }
+  }, [locations]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-center bg-cover" style={{ backgroundImage: `url(${worldMapImage})` }}>
